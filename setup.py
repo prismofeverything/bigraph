cce8b7fb00379f01ec96dbcd45a0a1d6f8c5d78e
Ryan Spangler <ryan.spangler@gmail.com>
2023-05-23 00:42:42 -0700

    first release

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup, find_packages
 
+
+VERSION = '0.0.1'
+
+
+with open('README.md', 'r') as readme:
+    description = readme.read()
+
+
 setup(
     name='bigraph',
-    version='0.0.1',
-    description='a python interface to Robin Milner\'s bigraph formalism',
+    version=VERSION,
     author='Ryan Spangler',
     author_email='ryan.spangler@gmail.com',
+    description='a python interface to Robin Milner\'s bigraph formalism',
     url='https://github.com/prismofeverything/bigraph',
     packages=find_packages(include=['bigraph', 'bigraph.*']),
+    python_requires=">=3.6",
     install_requires=[
         'fire',
         'parsimonious',
